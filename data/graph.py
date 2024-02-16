8c6f980b98b975944c618a300f65d8a4b27a5098
Rajashekar <rajureddymora@gmail.com>
2024-02-16 17:32:36 -0500

    Build dataclasses to handle message and edge outputs

---
+++ data/graph.py
@@ -0,0 +1,23 @@
+import dataclasses
+
+
+from typing import Union, Optional, List
+from pydantic import BaseModel
+
+from data.chat import Role
+from graph.node import BaseNode
+
+
+@dataclasses.dataclass
+class MessageOutput:
+    message: str
+    role: Role
+
+
+@dataclasses.dataclass
+class EdgeOutput:
+    should_continue: bool
+    result: Union[BaseModel, str]
+    message_output: Optional[List[MessageOutput]]
+    num_fails: int
+    next_node: BaseNode
